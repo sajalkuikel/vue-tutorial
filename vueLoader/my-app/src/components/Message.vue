b33ed8d98020d9a50128b6f1d0d62d6db13a9dfc
Sajal Kuikel <sajalkuikel.edu@gmail.com>
2021-08-14 19:56:31 +0545

    setting up project with vue loader

---
+++ vueLoader/my-app/src/components/Message.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="msg-box">
+    <p>
+        <slot></slot>
+    </p>
+  </div>
+
+</template>
+
+
+<script>
+  export default {
+
+  }
+
+</script>
+
+
+
+<style>
+  .msg-box{
+    background: gray;
+    padding: 10px;
+    border: 1px solid darkslateblue;
+    margin-bottom: 5px;
+  }
+</style>
